c33f7f5510ded064f0dab1c4e4193b9788b9fd30
ZiadDiab22 <zdiab6967@gmail.com>
2025-10-08 07:38:48 +0300

    image component

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
+//image component is better than image tag because it reduce the size of images which is making the loading faster
+import CloudImage from '../../../public/cloud-hosting.png';
+//if we want to use an image from global site or remote server then we need to edit config in next.config.js
+
 const AboutPage = () => {
-  return (
-    <div className="fix-height">About page زياد</div>
+  return (  
+    <section className="fix-height container m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
+      <div>
+        <Image src={CloudImage} alt='cloud' width={500} height={500} priority />
+      </div>
+    </section>
   )
 }
 
